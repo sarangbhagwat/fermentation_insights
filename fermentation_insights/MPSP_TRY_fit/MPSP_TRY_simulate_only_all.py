b99670123812458b423844be268e50571131ccbe
sarangbhagwat <sarangb2@illinois.edu>
2025-06-08 16:25:40 -0700

    finish implementing multiprocessing for TRY_analyses

--- fermentation_insights/MPSP_TRY_fit/MPSP_TRY_simulate_only_all.py
+++ fermentation_insights/MPSP_TRY_fit/MPSP_TRY_simulate_only_all.py
@@ -11,14 +11,16 @@ import os
 import multiprocessing
 import itertools
 
-
 def get_productivities(baseline_productivity,
                        multipliers=np.array([1., 0.2, 5.])):
     return multipliers*baseline_productivity
 
-products = ['TAL', 'TAL_SA', 
-            # 'HP', 'HP_hexanol', 
-            'succinic']
+products = [
+            'TAL', 'TAL_SA', 
+            'HP', 
+            'HP_hexanol', 
+            'succinic',
+            ]
 feedstocks = ['glucose', 'sugarcane', 'corn', 'cornstover']
 
 neutralizations_all = {
@@ -35,6 +37,7 @@ productivities_all = {
     'TAL': get_productivities(baseline_productivity=0.12),
     'TAL_SA': get_productivities(baseline_productivity=0.12),
     'HP': get_productivities(baseline_productivity=0.3255),
+    'HP_hexanol': get_productivities(baseline_productivity=0.3255),
     'succinic': get_productivities(baseline_productivity=0.6573)}
 
 steps = 3
@@ -44,6 +47,8 @@ processes = 12
 from biorefineries.succinic.analyses.TRY_analysis_FGI import run_TRY_analysis as run_TRY_analysis_succinic
 from biorefineries.TAL.analyses.fermentation.TRY_analysis_TAL_FGI import run_TRY_analysis as run_TRY_analysis_TAL
 from biorefineries.TAL.analyses.fermentation.TRY_analysis_SA_FGI import run_TRY_analysis as run_TRY_analysis_SA
+from biorefineries.HP.analyses.fermentation.TRY_analysis_FGI import run_TRY_analysis as run_TRY_analysis_HP
+from biorefineries.HP.analyses.fermentation.TRY_analysis_hexanol_FGI import run_TRY_analysis as run_TRY_analysis_HP_hexanol
 
 all_combs = []
 
@@ -67,6 +72,14 @@ def func(comb):
                 print_status_every_n_simulations=500,)
     elif product=='succinic':
         return run_TRY_analysis_succinic(feedstock=feedstock, neutralization=neutralization,
+                return_YT_arrays=True, plot=False, steps_for_default_YT=steps,
+                productivities=np.array([prod]))
+    elif product=='HP':
+        return run_TRY_analysis_HP(feedstock=feedstock, neutralization=neutralization,
+                return_YT_arrays=True, plot=False, steps_for_default_YT=steps,
+                productivities=np.array([prod]))
+    elif product=='HP_hexanol':
+        return run_TRY_analysis_HP_hexanol(feedstock=feedstock, neutralization=neutralization,
                 return_YT_arrays=True, plot=False, steps_for_default_YT=steps,
                 productivities=np.array([prod]))
     
@@ -114,7 +127,6 @@ if __name__ == '__main__':
         
             os.chdir('C://Users//saran//Documents//Academia//repository_clones//fermentation_insights//fermentation_insights//TRY_results')
             
-            os.chdir('C://Users//saran//Documents//Academia//repository_clones//fermentation_insights//fermentation_insights//TRY_results')
             np.save(f'{tag}_MPSP', results_metric_1)
             np.save(f'{tag}_GWP', results_metric_2)
             np.save(f'{tag}_FEC', results_metric_3)
@@ -127,5 +139,24 @@ if __name__ == '__main__':
             np.save(f'{tag}_productivities', productivities_all[product])
             
             print(f'\nSaved results for {tag}')
+            
+        elif product=='HP' or product=='HP_hexanol':
+            yields, titers, inflection_product_yields,\
+            results_metric_1, results_metric_2, results_metric_3,\
+            results_metric_4, results_metric_5, results_metric_6 = r
+        
+            os.chdir('C://Users//saran//Documents//Academia//repository_clones//fermentation_insights//fermentation_insights//TRY_results')
+            
+            np.save(f'{tag}_MPSP', results_metric_1)
+            np.save(f'{tag}_GWP', results_metric_2)
+            np.save(f'{tag}_FEC', results_metric_3)
+            np.save(f'{tag}_AOC', results_metric_4)
+            np.save(f'{tag}_TCI', results_metric_5)
+            np.save(f'{tag}_recoveries', results_metric_6)
+            np.save(f'{tag}_inflection_product_yields', inflection_product_yields)
+            np.save(f'{tag}_yields', yields)
+            np.save(f'{tag}_titers', titers)
+            np.save(f'{tag}_productivities', productivities_all[product])
+            
             
 
